1a8d28af30c85a21c60e205d29db8a19564d4e4a
rhkwj <rhkwj@naver.com>
2020-01-14 18:35:00 +0800

    https://www.youtube.com/watch?v=c8Syzl6KBk4&list=PLaXWdRaxFtVcgioOVP6UxFt43KQ6Gjur_&index=4 ep 3 finished

---
+++ InstagramClone/SignUpViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SignUpViewController.swift
+//  InstagramClone
+//
+//  Created by Kim Yeon Jeong on 2020/1/14.
+//  Copyright © 2020 Kim Yeon Jeong. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    
+    @IBAction func dismissOnClicked(_ sender: Any) { dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
