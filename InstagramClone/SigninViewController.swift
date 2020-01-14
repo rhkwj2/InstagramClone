6ed77e77c4bdda18b869768f858a0b2743809337
rhkwj <rhkwj@naver.com>
2020-01-14 18:13:07 +0800

    https://www.youtube.com/watch?v=fjz9aCQ6AfA&list=PLaXWdRaxFtVcgioOVP6UxFt43KQ6Gjur_&index=3 finished

---
+++ InstagramClone/SigninViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SigninViewController.swift
+//  InstagramClone
+//
+//  Created by Kim Yeon Jeong on 2020/1/14.
+//  Copyright © 2020 Kim Yeon Jeong. All rights reserved.
+//
+
+import UIKit
+
+class SigninViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
